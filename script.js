d70495c1ce8ee4fcb2cb0c3bb82ef0d58341c32a
MubMaj <mub.maj@gmail.com>
2024-03-08 16:12:13 +0530

    add package json

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
-const heading = React.createElement(
-  "h1",
-  { id: "heading" },
-  "Hello World... from React"
-);
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I am h1 Tag"),
-    React.createElement("h2", {}, "I am h2 Tag"),
+    React.createElement("h1", {}, "This is Learning React 📖"),
+    React.createElement("h2", {}, "by Mub"),
   ]),
   React.createElement("div", { id: "child2" }, [
     React.createElement("h1", {}, "I am h1 Tag"),
@@ -17,5 +15,3 @@ const parent = React.createElement("div", { id: "parent" }, [
 ]);
 
 root.render(parent);
-
-console.log(parent);
