2a60cd1b8e10308f43289c70f34447824467eed5
MubMaj <mub.maj@gmail.com>
2024-03-17 22:18:43 +0530

    add search feat

--- src/components/Body.js
+++ src/components/Body.js
@@ -3,7 +3,9 @@ import { useEffect, useState } from "react";
 import ShimmerComponent from "./Shimmer";
 
 const BodyComponent = () => {
-  let [listOfResturants2, setlistOfResturants2] = useState([]);
+  const [listOfResturants2, setlistOfResturants2] = useState([]);
+  const [filteredList, setfilteredList] = useState([]);
+  const [searchText, setsearchText] = useState([]);
 
   useEffect(() => {
     fetchData();
@@ -19,6 +21,22 @@ const BodyComponent = () => {
     setlistOfResturants2(
       json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
     );
+    setfilteredList(
+      json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+  };
+
+  const handleSearch = () => {
+    const searchTextString = typeof searchText === "string" ? searchText : "";
+    const filterlist = listOfResturants2.filter((res) =>
+      res.info.name.toLowerCase().includes(searchTextString.toLowerCase())
+    );
+    setfilteredList(filterlist);
+  };
+  const handleKeyPress = (e) => {
+    if (e.key === "Enter") {
+      handleSearch();
+    }
   };
 
   return listOfResturants2.length === 0 ? (
@@ -27,21 +45,43 @@ const BodyComponent = () => {
     <div>
       <main className="Body">
         <div className="filter">
-          <button
-            className="filter-btn"
-            onClick={() => {
-              setlistOfResturants2();
-              let filteredlist = listOfResturants2.filter(
-                (res) => res.info.avgRatingString > 4
-              );
-              setlistOfResturants2(filteredlist);
-            }}
-          >
-            Top Rated Restaurant
-          </button>
+          <div className="search-box">
+            <input
+              type="text"
+              className="filter-search"
+              value={searchText}
+              onChange={(e) => setsearchText(e.target.value)}
+              onKeyDown={handleKeyPress}
+            ></input>
+            <button className="filter-btn" onClick={handleSearch}>
+              Search
+            </button>
+          </div>
+          <div className="filter-container">
+            <button
+              className="filter-btn"
+              onClick={() => {
+                let filteredlist = listOfResturants2.filter(
+                  (res) => res.info.avgRatingString > 4
+                );
+                setfilteredList(filteredlist);
+              }}
+            >
+              Top Rated Restaurant
+            </button>
+            <button
+              className="filter-btn"
+              onClick={() => {
+                let filteredlist = listOfResturants2;
+                setfilteredList(filteredlist);
+              }}
+            >
+              Reset Restaurant
+            </button>
+          </div>
         </div>
         <div className="res-container">
-          {listOfResturants2.map((restu) => (
+          {filteredList.map((restu) => (
             <RestaurantCard key={restu.info.id} resData={restu} />
           ))}
         </div>
