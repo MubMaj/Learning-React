fcc3416ccbb29796eb5a5eadc0c43a52b300f1ed
MubMaj <mub.maj@gmail.com>
2024-04-13 15:39:04 +0530

    changed proxy

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,3 @@
-
 export const CDN_URL =
   "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
 
@@ -6,7 +5,7 @@ export const LOGO_URL =
   "https://png.pngtree.com/template/20191014/ourmid/pngtree-pin-food-delivery-map-location-delivery-logo-concept-image_318151.jpg";
 
 export const API_D_URL =
-  "https://proxy.cors.sh/https://www.swiggy.com/dapi/restaurants/list/v5?lat=25.4300572&lng=81.7869042";
+  "https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/restaurants/list/v5?lat=25.4300572&lng=81.7869042&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
 
 export const Res_API_URL =
-  "https://proxy.cors.sh/https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=25.4272113&lng=81.805925&restaurantId=";
+  "https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=25.4272113&lng=81.805925&restaurantId=";
